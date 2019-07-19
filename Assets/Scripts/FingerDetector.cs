f2506232eea3b9d3cd069b7ab3bce68e727a23bd
Jose Galvez <jjgalper@gmail.com>
2019-07-18 20:13:49 -0500

    Finger detection and node color

---
+++ Assets/Scripts/FingerDetector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerDetector : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Vector2 touchPos = Input.GetTouch(0).position;
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(touchPos);
+
+            transform.position = worldPos;
+            //Debug.Log(worldPos);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public bool painted;
+    public Vector2 worldPosition;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<SpriteRenderer>().color = Color.blue;
+    }
+
+
+}
