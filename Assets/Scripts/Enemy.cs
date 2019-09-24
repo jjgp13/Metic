ec53f83cd05b7ec01e674967419a45ac37201663
Jose Galvez <jjgalper@gmail.com>
2019-09-23 20:43:32 -0500

    Changes in art

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int result;
+    public float velocity;
+    public float timeInField;
+    public int pointsForKill;
+    public Sprite[] ballNumbers = new Sprite[9];
+}
