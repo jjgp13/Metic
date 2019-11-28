a82759e6c89f6bc8e820550c6258971f81a4f448
José Juan Gálvez <jjgalper@gmail.com>
2019-11-28 17:01:46 -0600

    alientype5

--- Assets/Scripts/EnemyScripts/AlienType5.cs
+++ Assets/Scripts/EnemyScripts/AlienType5.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class AlienType5 : Enemy, IMovable, ISetable
 {
+    public Vector2[] movePositions;
+    public Vector2 pointToMove;
+    public float distanceThreshold;
+
     private void Start()
     {
         SetEnemy();
         SetEnemyPoints();
+        pointToMove = movePositions[Random.Range(0, 3)];
     }
 
     private void Update()
@@ -23,7 +28,7 @@ public class AlienType5 : Enemy, IMovable, ISetable
         for (int i = 0; i < numberOfBalls; i++)
         {
             //From 1 to 9 -> 0 is 1 and 8 is nine
-            int tempNum = Random.Range(0, 9);
+            int tempNum = Random.Range(1, 9);
             transform.GetChild(i).GetComponent<SpriteRenderer>().sprite = NumbersController._instance.greenNumbers[tempNum];
             result *= tempNum + 1;
         }
@@ -31,6 +36,16 @@ public class AlienType5 : Enemy, IMovable, ISetable
 
     public void EnemyMovement()
     {
-
+        Debug.Log(Vector2.Distance(pointToMove, transform.position));
+        if (Vector2.Distance(pointToMove, transform.position) > distanceThreshold)
+        {
+            Vector2 position2d = transform.position;
+            Vector2 direction = pointToMove - position2d;
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            pointToMove = movePositions[Random.Range(0, 3)];
+        }
     }
 }
